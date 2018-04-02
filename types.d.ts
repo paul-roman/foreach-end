84984905df3b1e7f2e56603e8533ea9852833398
Paul Roman <paul.roman@epitech.eu>
2018-04-02 17:36:04 +0100

    Fix TypeScript definition

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
+declare module 'foreach-end';
+
 interface Array<T> {
 	forEachEnd(elementCallback: (value: T, done: () => void, index: number, array: T[]) => void,
 			   endCallback: () => void, thisArg?: any): void;
