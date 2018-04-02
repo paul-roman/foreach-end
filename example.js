67f7f31cd9682da0073b74cb1176cac77b83a417
Paul Roman <paul.roman@epitech.eu>
2018-04-02 17:28:11 +0100

    Update documentation
    Update README.md and examples

--- example.js
+++ example.js
@@ -1,7 +1,14 @@
-require('./index');
+require('./dist/index');
 
-[1, 2, 3].forEachEnd((value, done) => {
-	console.log(`${value} starting`);
+[1, 2, 3, 4].forEachEnd((value, done) => {
+	console.log(`${value} evaluated`);
+	done();
+}, () => {
+	console.log('ended!');
+});
+
+['a', 'b', 'c', 'd'].forEachEnd((value, done, index, array) => {
+	console.log(`${index + 1} / ${array.length} => ${value} starting`);
 	setTimeout(() => {
 		console.log(`${value} evaluated`);
 		done();
